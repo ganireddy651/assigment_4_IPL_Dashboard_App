89b4f8e6d5eab9dcceb85e62ca25c6db739a90f7
ganireddy651 <gajarla.ganesh1995@gmail.com>
2023-06-02 17:34:01 +0000

    styling changes for mobile break point

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -12,28 +12,32 @@ const LatestMatch = props => {
 
   return (
     <div className="latest-match-container">
-      <div className="sub-container">
-        <p>{competingTeam}</p>
-        <p>{date}</p>
-        <p>{venue}</p>
-        <p>{result}</p>
-      </div>
-      <div>
-        <img
-          className="ipl-team-logo"
-          src={competingTeamLogo}
-          alt={`latest match ${competingTeam}`}
-        />
-      </div>
-      <div className="result-container">
-        <p>First Innings</p>
-        <p>{firstInnings}</p>
-        <p>Second Innings</p>
-        <p>{secondInnings}</p>
-        <p>Man of The Match</p>
-        <p>{manOfTheMatch}</p>
-        <p>Umpires</p>
-        <p>{umpires}</p>
+      <div className="container">
+        <div className="sub-container">
+          <div>
+            <p className="team">{competingTeam}</p>
+            <p className="date">{date}</p>
+            <p className="venue">{venue}</p>
+            <p className="result-of-match">{result}</p>
+          </div>
+          <div className="image-container-ipl">
+            <img
+              className="ipl-team-logo"
+              src={competingTeamLogo}
+              alt={`latest match ${competingTeam}`}
+            />
+          </div>
+        </div>
+        <div className="result-container">
+          <p>First Innings</p>
+          <p>{firstInnings}</p>
+          <p>Second Innings</p>
+          <p>{secondInnings}</p>
+          <p>Man of The Match</p>
+          <p>{manOfTheMatch}</p>
+          <p>Umpires</p>
+          <p>{umpires}</p>
+        </div>
       </div>
     </div>
   )
